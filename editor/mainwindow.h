55c5d2dd887e04a084ee6d533543285a1d7ab0de
Erik Faye-Lund <kusmabite@gmail.com>
2016-01-26 10:55:16 +0100

    editor: trackView -> currentTrackView

--- editor/mainwindow.h
+++ editor/mainwindow.h
@@ -48,7 +48,7 @@ public:
 
 	QTabWidget *tabWidget;
 	QList<TrackView *> trackViews;
-	TrackView *defaultTrackView, *trackView;
+	TrackView *defaultTrackView, *currentTrackView;
 
 	QLabel *statusPos, *statusValue, *statusKeyType;
 	QMenu *fileMenu, *recentFilesMenu, *editMenu;
